81f48e834b4619fae71cd37c47c80ddf123d51dc
Arne Slabbinck <arne.slabbinck@gmail.com>
2024-01-10 16:40:45 +0100

    service cards

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,13 +7,42 @@ import { styles } from '../styles';
 import { services } from '../constants';
 import { fadeIn, textVariant } from '../utils/motion'
 
+// Component
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        // fadein from right, type spring, delay multiplied by index so it goes faster with more
+        // length 0.75
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        <div 
+          // Tilt options
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450
+          }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+        >
+          <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+          <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
+        </div>
+      </motion.div>
+    </Tilt>
+
+  )
+}
+
 const About = () => {
   return (
     // regular react fragment
     <>
       {/* function call to the text utility function we created in our motion div to animate */}
       <motion.div variants={textVariant()}>
-        <p classname={styles.sectionSubText}>Introduction</p>
+        <p className={styles.sectionSubText}>Introduction</p>
         <h2 className={styles.sectionHeadText}>Overview.</h2>
       </motion.div>
 
@@ -28,6 +57,23 @@ const About = () => {
         Let's work together to bring your ideas to life!
 
       </motion.p>  
+
+      {/* Cards */}
+      {/* margin top 20      gap-10 for space between cards*/}
+      <div className="mt-20 flex flex-wrap gap-10">
+
+        {/* Loop over services */}
+        {services.map((service, index) => (
+
+          // For each service we want to render a custom service card
+
+          // TOFIX: auto loop all attributes from services to the servicecard
+          <ServiceCard key={service.title} index={index} icon={service.icon} title={service.title}/>
+          //... services : spread all properties comming from the service we are currently looping over
+
+        ))}
+
+      </div>
 
     </>
   )
